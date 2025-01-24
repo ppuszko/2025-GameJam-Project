fcb1e73e6bdee4e381fc6a0045f67d4f724efd0f
Patryk Puszko <ppuszko3@gmail.com>
2025-01-24 19:51:47 +0100

    bubble displayed

--- Blow Me!/Blow Me!.cpp
+++ Blow Me!/Blow Me!.cpp
@@ -2,7 +2,56 @@
 
 #include "raylib.h"
 
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int screenWidth = 1200;
+    const int screenHeight = 800;
+
+
+    //sprite sheet clipping logic part1. 
+    int currentFrame{ 0 };
+    int frameCounter{ 0 };
+    int frameSpeed{ 4 };
+    //
+
+
+    Vector2 bubblePos{ 100.f, 100.f };
+
+    InitWindow(screenWidth, screenHeight, "Blow Me!");
+
+
+    Texture2D bubble = LoadTexture("../Assets/bubble.png");
+    //source rectangle defining frame position on sprite sheet
+    Rectangle frameRec = { 0.f, 0.f, bubble.width / 4, bubble.height };
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+
+
+        frameCounter++;
+
+        if (frameCounter >= (60 / frameSpeed))
+        {
+            frameCounter = 0;
+            currentFrame++;
+
+            if (currentFrame > 3) currentFrame = 0;
+            frameRec.x = currentFrame * bubble.width / 4;
+           
+        }
+        BeginDrawing();
+        ClearBackground(LIGHTGRAY);
+
+        DrawTextureRec(bubble, frameRec, bubblePos, WHITE);
+
+
+        EndDrawing();
+
+    }
+
+
 }
