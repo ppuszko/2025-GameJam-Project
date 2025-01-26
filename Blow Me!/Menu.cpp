12b94e8d00fb7ecd94e0e52888d9be173a630ee6
SzymokArch <94990211+SzymokArch@users.noreply.github.com>
2025-01-26 13:05:34 +0100

    Add files via upload

--- Blow Me!/Menu.cpp
+++ Blow Me!/Menu.cpp
@@ -1,50 +1,82 @@
-//#include <raylib.h>
-//
-//int main() {
-//  Texture logo = LoadTexture("../Assets/Logo.png");
-//  
-//  InitWindow(1200, 800, "Blow Me!");
-//  InitAudioDevice();
-//  Music music = LoadMusicStream("../Assets/music/muzyka1.mp3");
-//  PlayMusicStream(music);
-//
-//  Texture start = LoadTexture("../Assets/start.png");
-//  Texture exit = LoadTexture("../Assets/exit.png");
-//  Texture background = LoadTexture("../Assets/background/Ocean_2/5.png");
-//  
-//  while (!WindowShouldClose()) {
-//    UpdateMusicStream(music);
-//    BeginDrawing(); 
-//    {
-//      ClearBackground(WHITE);
-//      DrawTexturePro(background, {0, 0, (float)background.width, (float)background.height}, {0, 0, 1200, 800}, {0, 0}, 0, WHITE);
-//      DrawTextureEx(logo, {600 - logo.width * 2.f, 20}, 0, 4, WHITE);
-//      // start hovered
-//      if (GetMouseX() >= 600 - start.width/2.f && GetMouseX() <= 600 + start.width/2.f) {
-//        if (GetMouseY() >= 300 && GetMouseY() <= 300 + start.height * 2.0f) {
-//          DrawTexturePro(start, {start.width/2.f, 0, start.width/2.f, (float)start.height}, {600 - start.width/2.f, 300, (float)start.width, start.height * 2.f}, {0, 0}, 0, WHITE);
-//          DrawTexturePro(exit, {0, 0, start.width/2.f, (float)start.height}, {600 - start.width/2.f, 450, (float)start.width, start.height * 2.f}, {0, 0}, 0, WHITE);
-//        }
-//        // exit hovered
-//        else if (GetMouseY() >= 450 && GetMouseY() <= 450 + start.height * 2.0f) {
-//          DrawTexturePro(start, {0, 0, start.width/2.f, (float)start.height}, {600 - start.width/2.f, 300, (float)start.width, start.height * 2.f}, {0, 0}, 0, WHITE);
-//          DrawTexturePro(exit, {start.width/2.f, 0, start.width/2.f, (float)start.height}, {600 - start.width/2.f, 450, (float)start.width, start.height * 2.f}, {0, 0}, 0, WHITE);
-//          if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-//            return 0;
-//          }
-//        }
-//        else {
-//          DrawTexturePro(start, {0, 0, start.width/2.f, (float)start.height}, {600 - start.width/2.f, 300, (float)start.width, start.height * 2.f}, {0, 0}, 0, WHITE);
-//          DrawTexturePro(exit, {0, 0, start.width/2.f, (float)start.height}, {600 - start.width/2.f, 450, (float)start.width, start.height * 2.f}, {0, 0}, 0, WHITE);
-//        }
-//      }
-//      else {
-//        DrawTexturePro(start, {0, 0, start.width/2.f, (float)start.height}, {600 - start.width/2.f, 300, (float)start.width, start.height * 2.f}, {0, 0}, 0, WHITE);
-//        DrawTexturePro(exit, {0, 0, start.width/2.f, (float)start.height}, {600 - start.width/2.f, 450, (float)start.width, start.height * 2.f}, {0, 0}, 0, WHITE);
-//      }
-//      // DrawTextureEx(start, {600.f - start.width, 300}, 0, 2, WHITE);
-//    }
-//    EndDrawing();
-//  }
-//  return 0;
-//}
+#include "Menu.hpp"
+#include <cstdlib>
+#include <raylib.h>
+
+Menu::Menu() {
+  logo_txtr = LoadTexture("../Assets/Logo.png");
+  logo_dimensions = {(float)logo_txtr.width, (float)logo_txtr.height};
+  start_txtr = LoadTexture("../Assets/start.png");
+  exit_txtr = LoadTexture("../Assets/exit.png");
+  option_dimensions = {start_txtr.width/2.f, (float)start_txtr.height};
+}
+void Menu::reset_menu() {
+  exitflag = false;
+  startflag = false;
+}
+
+void Menu::check_options() {
+  if (GetMouseX() >= 600 - option_dimensions.x && GetMouseX() <= 600 + option_dimensions.x) {
+    if (GetMouseY() >= 300 && GetMouseY() <= 300 + option_dimensions.y * 2)
+      selected = START;
+    else if (GetMouseY() >= 450 && GetMouseY() <= 450 + option_dimensions.y * 2)
+      selected = EXIT;
+    else
+      selected = UNSELECTED;
+  }
+  else selected = UNSELECTED;
+}
+
+void Menu::draw_options() {
+  DrawTextureEx(logo_txtr, {600 - logo_dimensions.x * 2.f, 20}, 0, 4, WHITE);
+
+  Rectangle start_source = {
+    0,
+    0,
+    option_dimensions.x,
+    option_dimensions.y
+  };
+
+  Rectangle exit_source = {
+    0,
+    0,
+    option_dimensions.x,
+    option_dimensions.y
+  };
+
+  Rectangle start_dest = {
+    600 - option_dimensions.x,
+    300,
+    option_dimensions.x * 2,
+    option_dimensions.y * 2
+  };
+
+  Rectangle exit_dest = {
+    600 - option_dimensions.x,
+    450,
+    option_dimensions.x * 2,
+    option_dimensions.y * 2
+  };
+
+  switch (selected) {
+    case START:
+      start_source.x = option_dimensions.x;
+      exit_source.x = 0;
+      break;
+    case EXIT:
+      start_source.x = 0;
+      exit_source.x = option_dimensions.x;
+      break;
+    case UNSELECTED:
+      start_source.x = 0;
+      exit_source.x = 0;
+      break;
+  }
+  DrawTexturePro(start_txtr, start_source, start_dest, {0, 0}, 0, WHITE);
+  DrawTexturePro(exit_txtr, exit_source, exit_dest, {0, 0}, 0, WHITE);
+  
+  if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+    if (selected == START) startflag = true;
+    else if (selected == EXIT) exitflag = true;
+  }
+}
+
